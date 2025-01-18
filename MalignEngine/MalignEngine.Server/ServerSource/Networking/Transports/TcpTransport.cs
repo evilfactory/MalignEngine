e0571f18a4d2eddc365ef23906b41c18c199ab86
EvilFactory <evilfactory@users.noreply.github.com>
2025-01-17 21:02:13 -0300

    Move stuff arond in TcpTransport

--- MalignEngine/MalignEngine.Server/ServerSource/Networking/Transports/TcpTransport.cs
+++ MalignEngine/MalignEngine.Server/ServerSource/Networking/Transports/TcpTransport.cs
@@ -47,53 +47,6 @@ namespace MalignEngine
             AcceptClients();
         }
 
-        public async void AcceptClients()
-        {
-            while (server != null)
-            {
-                TcpClient client = await server.AcceptTcpClientAsync();
-                var connection = new NetworkConnection(CreateId());
-                connection.IsInvalid = false;
-                OnClientConnected?.Invoke(connection);
-                clients.Add(connection.Id, client);
-
-                HandleClient(client, connection);
-            }
-        }
-
-        public async void HandleClient(TcpClient client, NetworkConnection connection)
-        {
-            NetworkStream stream = client.GetStream();
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-
-            try
-            {
-                while (true)
-                {
-                    bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-                    if (bytesRead == 0)
-                    {
-                        DisconnectClient(connection, DisconnectReason.Unknown);
-                        break;
-                    }
-
-                    IReadMessage message = new ReadOnlyMessage(buffer, false, 0, bytesRead);
-                    message.Sender = connection;
-                    
-                    receiveQueue.Enqueue(new QueuedReceiveMessage
-                    {
-                        Message = message,
-                        Connection = connection
-                    });
-                }
-            }
-            catch (Exception exception)
-            {
-                DisconnectClient(connection, DisconnectReason.Unknown);
-            }
-        }
-
         public override void Shutdown()
         {
             server.Stop();
@@ -127,13 +80,6 @@ namespace MalignEngine
         }
 
 
-        private byte currentId = 0;
-        private byte CreateId()
-        {
-            currentId++;
-            return currentId;
-        }
-
         public override void DisconnectClient(NetworkConnection connection, DisconnectReason reason)
         {
             TcpClient client = clients[connection.Id];
@@ -143,6 +89,60 @@ namespace MalignEngine
             connection.IsInvalid = true;
 
             OnClientDisconnected?.Invoke(connection, reason);
+        }
+
+        private async void AcceptClients()
+        {
+            while (server != null)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+                var connection = new NetworkConnection(CreateId());
+                connection.IsInvalid = false;
+                OnClientConnected?.Invoke(connection);
+                clients.Add(connection.Id, client);
+
+                HandleClient(client, connection);
+            }
+        }
+
+        private async void HandleClient(TcpClient client, NetworkConnection connection)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            try
+            {
+                while (true)
+                {
+                    bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    if (bytesRead == 0)
+                    {
+                        DisconnectClient(connection, DisconnectReason.Unknown);
+                        break;
+                    }
+
+                    IReadMessage message = new ReadOnlyMessage(buffer, false, 0, bytesRead);
+                    message.Sender = connection;
+
+                    receiveQueue.Enqueue(new QueuedReceiveMessage
+                    {
+                        Message = message,
+                        Connection = connection
+                    });
+                }
+            }
+            catch (Exception exception)
+            {
+                DisconnectClient(connection, DisconnectReason.Unknown);
+            }
+        }
+
+        private byte currentId = 0;
+        private byte CreateId()
+        {
+            currentId++;
+            return currentId;
         }
     }
 }
