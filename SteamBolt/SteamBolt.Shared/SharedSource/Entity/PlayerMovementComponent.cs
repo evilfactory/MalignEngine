936583dc299a2e9e7953c6d1c874e3be2f232c75
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2026-07-05 15:35:35 -0300

    Fix transform being broken broky

--- SteamBolt/SteamBolt.Shared/SharedSource/Entity/PlayerMovementComponent.cs
+++ SteamBolt/SteamBolt.Shared/SharedSource/Entity/PlayerMovementComponent.cs
@@ -6,7 +6,9 @@ namespace SteamBolt;
 [Serializable]
 public struct PlayerMovementComponent : IComponent
 {
+    [DataField(nameof(MoveSpeed))]
     public float MoveSpeed;
+    [DataField(nameof(JumpForce))]
     public float JumpForce;
     public bool IsGrounded;
 }
